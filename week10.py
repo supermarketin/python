5a36e5c44f03cec38c4edf0444ef2cff92a5fac8
supermarketin <127291628+supermarketin@users.noreply.github.com>
2023-05-03 18:37:17 +0900

    Add files via upload

---
+++ week10.py
@@ -0,0 +1,62 @@
+# ###반복문 for
+# ## 리스트 x for
+
+# student = ['동건','민정','유림']
+# cnt = 1
+# for s in student:
+#     print(cnt,s)
+#     cnt += 1
+
+
+# ## for x 튜플
+
+# snack = ('뺴뺴로','허니버터칩','양파링','초코파이')
+# for c in snack:
+#     print(c, end=' ')
+
+
+
+# ### 딕셔너리 for
+# score ={'국':100, '영어':100 , '수학':100 ,'역사': 80}
+
+# # for d in score:
+# #     print(s, ':' , score[s] )
+
+# ### for x range
+
+# for i in range(10):
+#     print(i)
+
+
+# for i in range(1,10):
+#     print(i)
+
+# for i in range(1,101,2):
+#     print(i)
+
+# ###파놀리
+# nn = int(input('숫자 입력 :'))
+# for n in range(nn):
+#     print('python')
+
+
+# nn = int(input('숫자 입력 :'))
+# for i in range (2,nn,2):
+#     print(i,end = ' ')
+
+nn = int(input('숫자 입력 :'))
+for i in range (1,10):
+    print(f"{nn}x{i}={nn*i}")
+
+### Homework #######
+##1. 2단부터 9단까지 구구단을 출력하는 프로그램을 작성하시오.
+##2. 몇 가지 과목의 점수를 입력받아서 평균을 구하여 출력하고 평균이 80점 이상이면 "pass", 
+##   아니면 "fail"이라고 출력하는 프로그램을 작성하시오. (평균은 반올림하여 소수 첫째자리까지 출력)
+##3. #18. 정수를 입력받아 다음과 같이 순서쌍을 출력하는 프로그램을 작성하시오.
+## [입력] 4
+## [출력]
+## (1, 1) (1, 2) (1, 3) (1, 4)
+## (2, 1) (2, 2) (2, 3) (2, 4)
+## (3, 1) (3, 2) (3, 3) (3, 4)
+## (4, 1) (4, 2) (4, 3) (4, 4)
+
