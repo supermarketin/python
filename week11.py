18b74fcd8c95e57c57fc1addca957baa77e44891
supermarketin <127291628+supermarketin@users.noreply.github.com>
2023-05-24 19:11:09 +0900

    Add files via upload

--- week11.py
+++ week11.py
@@ -106,80 +106,137 @@
 # #이게 아래걸로 바뀜
 # n = [int(y) for y in m]
 # print(m)
-1
-m=input("text")
-n= input("NUMber")
-print(n+m)
-2
-s='과천코딩2022'
-print(s[2],s[3],s[4],s[5])
-33
-f= input('fruit name ')
-n=input ("how many? ")
-print(f"어제 {f}를 {n}깨 먹었씁니다")
-4
-t = input(" 아무거나 써 숫자: ")
-tt = input(" 아무거나 써 숫자: ")
-print(f"{t}+{tt} = (t+tt),{t}-{tt} = (t-tt),{t}*{tt} = (t*tt),{t}/{tt} = (t/tt)")
-5
-mum = input(" 아무거나 써 숫자: ")
-if mum % 2 == 0: 
-    print('even')
-else :
-    print('odd')
-6
-p = input(" 아무거나 써 숫자: ")
 
-for a in range(1, input_num+1 ):
-    if input_num % a == 0:
-        print(a,"(은)는 약수")
-       
-7
-u = input(" 아무거나 써 숫자: ")
-v=0
-for i in range(2,a)
-    if a/i==0:
-        b =1
-if b==1 or a==1 or a==0
-    print('false')
-else
-    print("true")
-8    
-g = input(" 2-9 사이의 숫자를 써라 안그러ㅕㄴ 청개구리: ")
-for i in range(1,10)
-    print( g,'*' , i, '=' , g*i)
-9
-print('1부터 n까지 정수의 합을 구합니다.')
-n = int(input('n값을 입력하세요.: '))
-sum = 0
-i = 1
-while i <= n:
-  sum = sum + i    
-  i = i + 1        
-print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
-10
+#                                        I'm gunna pack my things and go to your mind
+#                                     cause I know your not OK, but your saying " I'm OK "
+#                                    I hope you think about yourself , and I will help it
+#                                         cause you are a paitent and I am a doctor
+
+
+# star= int(input('숫자입력'))
+
+# for i in range(1,star+1):
+#     for s in range (star , i , -1):
+#         print(' ', end = '')
+#     print('*'*(i*2-1))
+
+
+
+# star= int(input('숫자입력'))
+
+# for s in range (1,star+1):
+#     print('*'*s)
+
+# for i in range(star-1,0,-1):
+#     print("*"*i)
+
+
+# star= int(input('숫자입력'))
+
+# for l in range(1,star*2):
+#     if l <= star:
+#         print('*'*l)
+#     else:
+#         print('*'*(star*2-l))
+
+# n= int(input('숫자입력'))
+# j=0
+# for l in range(1,n+1):
+#     for i in range(1,n+1):
+#         print(l , end = ' ') 
+#     print()
+# #1
+# m=input("text")
+# n= int(input("NUMber"))
+# print(m,end=' ')
+# print(n)
+# print(f'{n}{m}')
+# #2
+# s='과천코딩2022'
+# print(s[2:6])
+# print(s[-6:-2])
+# #3
+# f= input('fruit name ')
+# n=input ("how many? ")
+# print(f"어제 {f}를 {n}깨 먹었씁니다")
+# #4
+# t = int(input(" 아무거나 써 숫자: "))
+# tt = int(input(" 아무거나 써 숫자: "))
+# print(f"{t}+{tt} = {t+tt},{t}-{tt} = {t-tt},{t}*{tt} = {t*tt},{t}/{tt} = {t/tt:.2}")
+# #5
+# mum = int(input(" 아무거나 써 숫자: "))
+# if mum % 2 == 0:print('even')
+# elif mum%2 !=0:print('odd')
+# else: print('dd')
+# #6
+# p = input(" 아무거나 써 숫자: ")
+
+# for a in range(1,p+1):
+#     if p % a == 0: print(a,"(은)는 약수")
+#7
+# u = int(input(" 아무거나 써 숫자: "))
+# v=0
+# for i in range(1,u+1):
+#     if u%i==0:
+#         v +=1
+# if u==2:
+#     print('t')
+# else:
+#     print("f")
+# #8    
+# g = input(" 2-9 사이의 숫자를 써라 안그러ㅕㄴ 청개구리: ")
+# for i in range(1,10):
+#     print( g,'*' , i, '=' , g*i)
+# #9
+# n = int(input('n값을 입력하세요.: '))
+# sum = 0
+# i = 1
+# while i <= n:
+#     sum = sum + i    
+#     i = i + 1        
+# print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+
+# for i in range(1,n+1):
+#     sum += i
+# print(sum)
+# #10
+# n = int(input("Input number: "))
+# for i in range(1, n+1) :
+# 	s = str(i)
+# 	count = 0
+# 	for x in s :
+#     	# 3, 6, 9 문자가 있으면
+# 		if (x=='3') or (x=='6') or (x=='9') :
+# 			count += 1
+# 	if count == 0 :
+# 		print(i, end=' ')
+# 	else :
+# 		# count 수만큼 X를 출력, 출력 글자 띄기로 구분
+# 		print(count*'X', end=' ')
+
+
+# for i in range(1,n+1):
+#     if i%10 == 3 or  i%10 == 6 or  i%10 == 9:
+#         print('X', end=' ')
+#     elif i//10 == 3 or  i//10 == 6 or  i//10 == 9:
+#         print('X', end=' ')
+#     else:
+#         print(i,end=' ')
+# #11
+# for l in range(5):
+#     for i in range(1,6):
+#         print(i+l , end='')
+#     print()
+# # 4
+# #12
 n = int(input("Input number: "))
-for i in range(1, n+1) :
-	s = str(i)
-	count = 0
-	for x in s :
-    	# 3, 6, 9 문자가 있으면
-		if (x=='3') or (x=='6') or (x=='9') :
-			count += 1
-	if count == 0 :
-		print(i, end=' ')
-	else :
-		# count 수만큼 X를 출력, 출력 글자 띄기로 구분
-		print(count*'X', end=' ')
-12
-n = int(input("Input number: "))
-for i in range(n): # 정방향
-    print(' ' * ((n-1) - i) + '*' * (i * 2 + 1))
-for i in range(n): # 역방향
-    print(' ' * i + '*' * ((n*2-1) - (2 * i)))
-    
-        
+# for i in range(n): # 역방향
+#     print(' ' * i + '*' * ((n*2-1) - (2 * i)))
+# for s in range(2, n+1): # 정방향
+#     print(' ' * ((n*2-n)-s) + '*' * (s * 2 -1 ))
 
-
-
-
+for i in range(n*2-1):#전체 줄 수 아이 가 줄 수
+    if i < n:
+        print(' '*i + '*'*(2*(n-i)-1))#역삼각
+    else:
+        print(' '*((n*2-2)-i)+ '*'*((i-n+1)*2+1)) #정삼각
